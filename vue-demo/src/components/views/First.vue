0f0b3e0f6e909f014d01a3b33392fca543c8d80c
xiongyu <xiongyu132@gmail.com>
2021-09-23 16:10:36 +0800

    initial

---
+++ vue-demo/src/components/views/First.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="first-app">
+    {{ msg }}
+    <confirm text="注册" @message="getMessage"></confirm>
+    <p><router-link to="/second">To second page</router-link></p>
+  </div>
+</template>
+
+<script>
+import Confirm from "../sub/Confirm";
+export default {
+  name: "First",
+  components: {
+    Confirm,
+  },
+  data() {
+    return {
+      msg: "Welcome to FirstApp",
+    };
+  },
+  methods: {
+    getMessage(val) {
+      alert(val);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue-demo/src/components/views/Second.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="second-app">
+    {{ msg }}
+    <confirm text="注册" @message="getMessage"></confirm>
+  </div>
+</template>
+
+<script>
+import Confirm from "../sub/Confirm";
+export default {
+  name: "Second",
+  components: {
+    Confirm,
+  },
+  data() {
+    return {
+      msg: "Welcome to SecondApp",
+    };
+  },
+  methods: {
+    getMessage(val) {
+      alert(val);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
